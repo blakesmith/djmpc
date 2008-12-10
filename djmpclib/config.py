fc8c4336a9fb21cda0a36f14a1b27724e6ea84c5
Blake Smith <blake@safire.(none)>
2008-12-10 13:02:21 -0500

    djmpc_config now properly handled in user's $HOME. README updated with more usage and functions.

--- djmpclib/config.py
+++
@@ -1,4 +0,0 @@
-mpd_address = "localhost"
-mpd_port = 6600
-music_directory = "/home/blake/.mpd/music"
-cue_directory = "/home/blake/cues/"
---
+++ djmpclib/djmpc_config.py
@@ -0,0 +1,4 @@
+mpd_address = "localhost"
+mpd_port = 6600
+music_directory = "/home/user/.mpd/music"
+cue_directory = "/home/user/cues/"
