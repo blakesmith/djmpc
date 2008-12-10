de23305e8ad90d13dd571e56a3f58633caa61874
Blake Smith <blake@safire.djblithe.com>
2008-12-10 01:52:31 -0500

    Make djmpc a standalone installable package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='djmpc',
+        version='beta-0.4',
+        description='An ncurses MPD client with cuesheet support',
+        author='Blake Smith',
+        author_email='blakesmith0@gmail.com',
+        url='http://djblithe.com/page/djmpc',
+        license='GPLv2',
+        packages=['djmpclib'],
+        scripts=['djmpclib/djmpc'],
+        )
