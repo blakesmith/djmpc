bf712182875c7724e4e886c10b7d7ba939f32cd5
Blake Smith <blakesmith0@gmail.com>
2009-01-06 17:20:51 -0500

    setup.py: version should report the latest tag rather than current commit.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
 def get_head_commit():
     head = open(".git/HEAD").read()[5:-1]
     head_commit = open(".git/" + head).read()[:10]
     return head_commit
 
+def get_recent_tag():
+    return os.popen("git tag | tail -n 1").read().rsplit("-")[1][:-1]
+
 setup(name='djmpc',
-        version=get_head_commit(),
+        version=get_recent_tag(),
         description='An ncurses MPD client with cuesheet support',
         author='Blake Smith',
         author_email='blakesmith0@gmail.com',
