261c6fe2a8f1b22c2d87b1dcdc71ec895f5a7b29
Blake Smith <blakesmith0@gmail.com>
2009-01-06 15:53:20 -0500

    setup.py: get_head_commit() added to retrieve the current commit version.

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@
 
 from distutils.core import setup
 
+def get_head_commit():
+    head = open(".git/HEAD").read()[5:-1]
+    head_commit = open(".git/" + head).read()[:10]
+    return head_commit
+
 setup(name='djmpc',
-        version='beta-0.4',
+        version=get_head_commit(),
         description='An ncurses MPD client with cuesheet support',
         author='Blake Smith',
         author_email='blakesmith0@gmail.com',
